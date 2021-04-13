351efccf817a052fc9d89666cc21c351ae1049ef
amoong <moo1323@naver.com>
2021-04-13 20:55:53 +0900

    Preview Images

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { dbService } from "fbase";
 import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
+  const fileRef = useRef(null);
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState(null);
 
   const getNweets = async () => {
     const dbNweets = await dbService.collection("nweets").get();
@@ -49,6 +51,25 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = event => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = finishedEvent => {
+      setAttachment(reader.result);
+    };
+
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => {
+    setAttachment(null);
+    fileRef.current.value = "";
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -59,7 +80,19 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input
+          ref={fileRef}
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+        />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="attachment" width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map(nweet => (
