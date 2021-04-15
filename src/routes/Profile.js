07b9cf7f37b089f7ad016c61f9277cb13df64eb4
amoong <moo1323@naver.com>
2021-04-15 20:56:42 +0900

    Get my own nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,24 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const onLogOutLick = () => {
     authService.signOut();
   };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+
+    nweets.docs.map(doc => console.log(doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
