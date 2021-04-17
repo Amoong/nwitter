ba564abbe2c0bde182a0423cb048faecdb9cbfcf
Amoong <moo1323@naver.com>
2021-04-17 15:28:59 +0900

    Update profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,43 @@
 import { authService } from "fbase";
-import React from "react";
+import React, { useState } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj, history }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogOutLick = () => {
     authService.signOut();
+    history.push("/");
+  };
+
+  const onChnage = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName === newDisplayName) {
+      return;
+    }
+
+    await userObj.updateProfile({
+      displayName: newDisplayName,
+    });
   };
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Diplay name"
+          onChange={onChnage}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutLick}>Log Out</button>
     </>
   );
